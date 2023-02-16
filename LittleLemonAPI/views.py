cfc8a5316e346f97b33d693e25cc4f8ff94331fb
Ayodeji Yekeen <ayodejiyou@gmail.com>
2023-02-16 11:12:37 +0100

    Update views.py

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -63,7 +63,7 @@ class GroupViewSet(viewsets.ViewSet):
     permission_classes = [IsAdminUser]
     def list(self, request):
         users = User.objects.all().filter(groups__name='Manager')
-        items = UserSerilializer(users, many=True)
+        items = UserSerializer(users, many=True)
         return Response(items.data)
 
     def create(self, request):
@@ -83,7 +83,7 @@ class DeliveryCrewViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
     def list(self, request):
         users = User.objects.all().filter(groups__name='Delivery Crew')
-        items = UserSerilializer(users, many=True)
+        items = UserSerializer(users, many=True)
         return Response(items.data)
 
     def create(self, request):
